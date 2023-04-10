7c20999caf496e5927838cf36dd93ed81cee9b34
Elon <vilotstarbiss@gmail.com>
2023-04-10 22:49:58 +0100

    Add new endpoint

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,7 +1,8 @@
 export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
+const churchlessAPIEndpoint = 'https://free.churchless.tech/v1/chat/completions';
 const customAPIEndpoint =
-  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
+  import.meta.env.VITE_CUSTOM_API_ENDPOINT || officialAPIEndpoint;
 export const defaultAPIEndpoint =
-  import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
+  import.meta.env.VITE_DEFAULT_API_ENDPOINT || churchlessAPIEndpoint;
 
-export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint];
+export const availableEndpoints = [churchlessAPIEndpoint, officialAPIEndpoint, customAPIEndpoint];
